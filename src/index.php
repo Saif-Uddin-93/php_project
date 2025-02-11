2bc1c814756fec93f3d5a40b1106b47222470cf6
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-02-11 15:33:56 +0000

    tidied code

--- src/index.php
+++ src/index.php
@@ -10,36 +10,6 @@
     <div class="max-viewport yellow-fill background">
     </div>
     <?php
-    // try {
-    //     // Connect to the PostgreSQL database
-    //     $pdo = new PDO('pgsql:host=192.168.1.205;port=5433;dbname=postgres', 'postgres', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    //     // $connection = pg_connect("host=192.168.1.205 db_name=postgres user=postgres");
-    //     // Define the SQL query to fetch records
-    //     $sql = 'SELECT * FROM jamaat_times';
-
-    //     // Execute the query
-    //     $stmt = $pdo->query($sql);
-
-    //     // Fetch the records
-    //     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    //     // Display the records
-    // Display the table headers
-    //     $headers = array_keys($records[0]);
-    //     echo '<table border="1">';
-    //     echo '<tr>';
-    //     foreach ($headers as $header) {
-    //         echo '<th>' . $header . '</th>';
-    //     }
-    //     echo '</tr>';
-    //     foreach ($records as $record) {
-    //         echo 'Record: ' . implode(', ', $record) . "<br>";
-    //     }
-    // } catch (PDOException $e) {
-    //     echo "Error fetching records: " . $e->getMessage();
-    // }
-    ?>
-    <?php
     // Connect to the PostgreSQL database
     try {
         $conn = pg_connect("host=192.168.1.205 port=5433 dbname=postgres user=postgres");
@@ -52,8 +22,7 @@
         // Close the connection
         pg_close($conn);
 
-        // Display the table headers
-        $headers = pg_field_name($query_response, 0);
+        // Display the table
         $numFields = pg_num_fields($query_response);
         $numRows = pg_num_rows($query_response);
         echo '<table border="1">';
@@ -67,30 +36,10 @@
             echo "<tr>" . "<td>" . $row['date'] . "</td><td>" . $row['day'] . "</td><td>" . $row['fajr'] . "</td><td>" . $row['zuhr'] . "</td><td>" . $row['asr'] . "</td><td>" . $row['maghrib'] . "</td><td>" . $row['isha'] . "</td></tr>";
         }
 
-        // Fetch the records
-        // while ($row = pg_fetch_assoc($query_response)) {
-        //     echo $row['column1'] . " test " . $row['column2'] . " " . $row['column3'] . "<br>";
-        // }
-
-        // // Close the connection
-        // pg_close($conn);
     } catch (Exception $e) {
         echo "Error connecting to database: " . $e->getMessage();
         exit;
     }
-    ?>
-    <?php
-    // // Display the table headers
-    // // $headers = pg_field_name($query_response, 0);
-    // // $numFields = pg_num_fields($query_response);
-    // echo '<br>';
-    // echo '<table border="1">';
-    // echo '<tr>';
-    // for ($i = 0; $i < $numFields; $i++) {
-    //     echo '<th>' . pg_field_name($query_response, $i) . '</th>';
-    // }
-    // echo '</tr>';
-    // echo '<br>';
     ?>
 </body>
 </html>
